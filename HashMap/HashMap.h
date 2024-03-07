b0fd899a87b829ec65b6872d9e8d349f7cfc66b1
Leison Gao <leisongao2005@g.ucla.edu>
2024-03-06 20:47:53 -0800

    working hashmap class

--- HashMap/HashMap.h
+++ HashMap/HashMap.h
@@ -8,6 +8,7 @@
 #ifndef geodb_h
 #define geodb_h
 
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -16,7 +17,7 @@ class HashMap
 {
 public:
     HashMap(double max_load = 0.75); // constructor
-    ~HashMap(); // destructor; deletes all of the items in the hashmap
+    ~HashMap() {}; // destructor; deletes all of the items in the hashmap
     int size() const; // return the number of associations in the hashmap
     // The insert method associates one item (key) with another (value).
     // If no association currently exists with that key, this method inserts
@@ -35,11 +36,158 @@ public:
     // the hashmap and map it to the default value of type T. Then it will return a
     // reference to the newly created value in the map.
     T& operator[](const std::string& key);
+    
+    HashMap(const HashMap&) = delete;
+    HashMap& operator=(const HashMap&) = delete;
+    
 private:
     struct Node {
-        std::string m_akey;
-        Node* m_next
+        std::string m_key;
+        T m_val;
+        Node* next;
+    };
+    
+    std::vector<Node*> m_nodes {10};
+    double m_max_load;
+    int m_size;
+    int m_numBuckets;
+    
+    size_t hashString(std::string s, bool rehash = false) const;
+    
+    double loadFactor() {
+        return (double)m_size / (double)m_numBuckets;
     }
-    vector<
+    
+    void reallocate();
 };
+
+template <typename T>
+size_t HashMap<T>::hashString(std::string s, bool rehash) const{
+    size_t x = std::hash<std::string>()(s) % (m_numBuckets * (1 + rehash));
+    // if rehashing, double buckets
+    return x;
+}
+
+template <typename T>
+HashMap<T>::HashMap(double max_load) {
+    if (max_load <= 0)
+        m_max_load = 0.75;
+    // make sure max_load is correct
+    
+    m_max_load = max_load;
+    m_size = 0;
+    m_numBuckets = 10;
+}
+
+template <typename T>
+int HashMap<T>::size() const {
+    return m_size;
+}
+
+template <typename T>
+void HashMap<T>::reallocate() {
+    std::vector<Node*> temp {static_cast<size_t>(2*m_numBuckets)};
+    
+    for (int i = 0; i < m_numBuckets; i ++) { // iterate over original hashtable
+        if (m_nodes.at(i) != nullptr) {
+            Node* p = m_nodes.at(i);
+            size_t hashKey = hashString(p->m_key, true); // rehash current item
+            temp.at(hashKey) = p; // store item in new hashtable
+            
+            Node* next_p = p->next; // store next value
+            p->next = nullptr; // make sure no longer linked
+            
+            while (next_p != nullptr) { // must check entire linked list
+                hashKey = hashString(next_p->m_key, true);
+                temp.at(hashKey) = next_p;
+            }
+        }
+    }
+    m_nodes = temp; // vector assignment operator should free memory of old hashmap
+}
+
+template <typename T>
+void HashMap<T>::insert(const std::string& key, const T& value) {
+    if (loadFactor() > m_max_load) {
+        reallocate();
+    }
+    size_t hashKey = hashString(key);
+    if (m_nodes.at(hashKey) == nullptr) { // when bucket is empty, create new node
+        Node* temp = new Node;
+        temp->m_key = key;
+        temp->m_val = value;
+        temp->next = nullptr;
+        m_nodes.at(hashKey) = temp;
+        m_size ++;
+    }
+    else { // bucket is not empty, must first trace to end of linked list
+        Node* p = m_nodes.at(hashKey);
+        while (p->next != nullptr) {
+            if (p->m_key == key) {
+                p->m_val = value; // reassigment if duplicate
+                return;
+            }
+            p = p->next;
+        }
+        Node* temp = new Node;
+        temp->m_key = key;
+        temp->m_val = value;
+        temp->next = nullptr;
+        p->next = temp;
+        m_size ++;
+    }
+}
+
+template <typename T>
+T* HashMap<T>::find(const std::string& key) const {
+    size_t hashKey = hashString(key);
+    if (m_nodes.at(hashKey) == nullptr) {
+        return nullptr;
+    }
+    else {
+        Node* p = m_nodes.at(hashKey);
+        if (p->m_key == key) {
+            return &p->m_val; // passing by reference because pointer value return type
+        }
+        while (p->next != nullptr) {
+            if (p->m_key == key) {
+                return &p->m_val;
+            }
+            p = p->next;
+        }
+        return nullptr;
+    }
+}
+
+template <typename T>
+T& HashMap<T>::operator[](const std::string& key) {
+    size_t hashKey = hashString(key);
+    if (m_nodes.at(hashKey) == nullptr) {
+        Node* temp = new Node;
+        temp->m_key = key;
+        temp->next = nullptr;
+        m_nodes.at(hashKey) = temp;
+        m_size ++;
+        return temp->m_val;
+    }
+    else {
+        Node* p = m_nodes.at(hashKey);
+        if (p->m_key == key) {
+            return p->m_val;
+        }
+        while (p->next != nullptr) {
+            if (p->m_key == key) {
+                return p->m_val;
+            }
+            p = p->next;
+        }
+        Node* temp = new Node;
+        temp->m_key = key;
+        temp->next = nullptr;
+        p->next = temp;
+        m_size ++;
+        return temp->m_val;
+    }
+}
+
 #endif /* geodb_h */
--- HashMap/main.cpp
+++ HashMap/main.cpp
@@ -6,9 +6,35 @@
 //
 
 #include <iostream>
+#include "HashMap.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+void foo()
+{
+    // Define a hashmap that maps strings to doubles and has a maximum
+    // load factor of 0.3. It will initially have 10 buckets when empty.
+    HashMap<double> nameToGPA(0.3);
+    // Add new items to the hashmap. Inserting the third item will cause
+    // the hashmap to increase the number of buckets (since the maximum
+    // load factor is 0.3), forcing a rehash of all items.
+    nameToGPA.insert("Carey", 3.5); // Carey has a 3.5 GPA
+    nameToGPA.insert("David", 2.99); // David needs to up his game
+    cout << nameToGPA.size() << endl;
+    cout << nameToGPA["Carey"] << endl;
+    cout << nameToGPA["David"] << endl;
+    // you can also use brackets like C++'s unordered_map!
+    nameToGPA["Annie"] = 3.85; // Adds Annie, who has the highest GPA of all
+    double* davidsGPA = nameToGPA.find("David");
+    if (davidsGPA != nullptr) {
+        *davidsGPA = 3.1; // after a re-grade of David's exam, update 2.99 -> 3.1
+        cout << "updated david gpa" << endl;
+    }
+//    cout << nameToGPA.find("David") << endl;
+    nameToGPA.insert("Carey", 4.0); // Carey deserves a 4.0
+    // sees if linda is in the map; if not, creates a new entry for linda in map
+    std::cout << nameToGPA["Linda"] << endl; // prints zero
+}
+
+int main() {
+    foo();
 }
